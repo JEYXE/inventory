3037e1a176289a57b490e18d2f738c21c113a3e8
Jorge Caldas <jorge.caldas@outlook.com>
2024-09-01 18:20:45 -0500

    Inicio del proyecto

---
+++ src/test/java/com/fontebo/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fontebo.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
